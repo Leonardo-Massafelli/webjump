1b9d4a71498a60b9ef39d47a5293539eba6f80a8
Leonardo Massafelli <leonardo.melo@webjump.com.br>
2022-02-03 15:48:08 -0300

    creating automotive theme

---
+++ app/design/frontend/Automotive_Theme/Automotive/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Automotive_Theme/Automotive',
+    __DIR__
+);
