610abadc0c2aafb9f936cd9721d0a62b1ff7ab81
Kelvyn <kelvyn.santos@webjump.com.br>
2022-02-21 15:55:43 -0300

    final theme

--- app/design/frontend/Automotive/principal_theme/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Automotive/principal_theme',
-    __DIR__
-);
