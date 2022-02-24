3adc6ca03fba8247a8e3bfd5898fb723be837825
Patrick Barbosa <patrick_souza98@hotmail.com>
2022-02-24 12:37:46 -0300

    Traslating color frontend labels

--- app/code/Webjump/Configuration/Setup/Patch/Data/TranslateAttrProduct.php
+++ app/code/Webjump/Configuration/Setup/Patch/Data/TranslateAttrProduct.php
@@ -11,6 +11,8 @@ use Magento\Store\Api\StoreRepositoryInterface;
 
 class TranslateAttrProduct implements DataPatchInterface {
 
+    CONST COLOR_LABLE = 'Color';
+
     private $moduleDataSetup;
     private $eavSetupFactory;
     private $productAttributeRepository;
@@ -38,11 +40,26 @@ class TranslateAttrProduct implements DataPatchInterface {
 
         $this->translateLabel(CreateWebsites::FESTA_EN_STORE_CODE, AddFestaAttributeProducts::FESTA_TEMA, 'Theme');
         $this->translateLabel(CreateWebsites::FESTA_EN_STORE_CODE, AddFestaAttributeProducts::FESTA_QTD, 'Quantity per package');
-        $this->translateLabel(CreateWebsites::FESTA_EN_STORE_CODE, AddFestaAttributeProducts::FESTA_COLOR, 'Color');
 
         $this->translateLabel(CreateWebsites::AUTOMOTIVO_EN_STORE_CODE, AddAutomotivoAttributeProducts::AUTOMOTIVO_AUTO, 'Automatic');
         $this->translateLabel(CreateWebsites::AUTOMOTIVO_EN_STORE_CODE, AddAutomotivoAttributeProducts::AUTOMOTIVO_AR_CONDICIONADO, 'Air conditioning');
-        $this->translateLabel(CreateWebsites::AUTOMOTIVO_EN_STORE_CODE, AddAutomotivoAttributeProducts::AUTOMOTIVO_COLOR, 'Color');
+
+        //TRANSLATING COLOR FRONTEND LABEL
+        $partyEnId = $this->storeRepository->get(CreateWebsites::FESTA_EN_STORE_CODE)->getId();
+        $automotiveEnId = $this->storeRepository->get(CreateWebsites::AUTOMOTIVO_EN_STORE_CODE)->getId();
+        $attr = $this->productAttributeRepository->get(AddFestaAttributeProducts::FESTA_COLOR);
+
+        $frontendLabels = [
+            $this->attributeFrontendLabelInterfaceFactory->create()
+                ->setStoreId($partyEnId)
+                ->setLabel(self::COLOR_LABLE),
+            $this->attributeFrontendLabelInterfaceFactory->create()
+                ->setStoreId($automotiveEnId)
+                ->setLabel(self::COLOR_LABLE),
+        ];
+
+        $attr->setFrontendLabels($frontendLabels);
+        $this->productAttributeRepository->save($attr);
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
