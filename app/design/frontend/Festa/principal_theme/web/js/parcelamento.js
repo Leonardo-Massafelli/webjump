25ccc284f06480cd3f14fc6460974ce3e8a9f7f0
Leonardo Massafelli <leonardo.melo@webjump.com.br>
2022-03-02 20:59:42 -0300

    Fixing installment JS

--- app/design/frontend/Festa/principal_theme/web/js/parcelamento.js
+++ app/design/frontend/Festa/principal_theme/web/js/parcelamento.js
@@ -2,40 +2,87 @@ document.addEventListener("DOMContentLoaded", () => {
     const price = document.querySelector(".price").textContent;
     const installment = document.querySelector(".parcelamento");
 
-    const onlyNumbers = price.substring(2);
+    let onlyNumbersBR;
+    let onlyNumbersEN;
 
-    const regReplace = onlyNumbers.replace(/\D/, ".");
-
-    const strToNum = Number(regReplace);
-    console.log(strToNum);
-
-    const installmentRule = (strToNum / 10).toFixed(2);
-    console.log(installmentRule);
-
-    const numToStr = String(installmentRule);
-    console.log(numToStr);
-
-    if (numToStr.length >= 5) {
-        const points = numToStr.substring(0, 2);
-        const rest = numToStr.substring(2, 5);
-        const formatedPrice = `${points}${rest}`;
-
-        const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${formatedPrice}`;
-
-        installment.textContent = installmentRender;
-    } else if (numToStr.length === 4) {
-        const points = numToStr.substring(0, 2);
-        const rest = numToStr.substring(2, 4);
-        const formatedPrice = `${points}${rest}`;
-        const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${formatedPrice}`;
-
-        installment.textContent = installmentRender;
+    if (price.includes("R$")) {
+        onlyNumbersBR = price.substring(2);
     } else {
-        const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${numToStr.substring(
-            0,
-            2
-        )},00`;
+        onlyNumbersEN = price.substring(1);
+    }
 
-        installment.textContent = installmentRender;
+    if (onlyNumbersBR) {
+        const regReplaceBR = onlyNumbersBR.replace(/\D/g, "");
+
+        const strToNum = Number(regReplaceBR);
+
+        const installmentRule = (strToNum / 10).toFixed(2);
+
+        const numToStr = String(installmentRule);
+
+        if (numToStr.length > 8) {
+            const points = numToStr.substring(0, 2);
+            const rest = numToStr.substring(2, 5);
+            const formatedPrice = `${points}.${rest}`;
+
+            const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${formatedPrice}`;
+
+            installment.textContent = installmentRender;
+        } else if (numToStr.length === 8) {
+            const points = numToStr.substring(0, 3);
+            const rest = numToStr.substring(6, 8);
+            const formatedPrice = `${points}.${rest}`;
+            const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${formatedPrice}`;
+
+            installment.textContent = installmentRender;
+        } else {
+            const installmentRender = `Parcele em até 10x com parcela mínima de R$ ${numToStr.substring(
+                0,
+                2
+            )}.00`;
+
+            installment.textContent = installmentRender;
+        }
+    } else {
+        const regReplaceEN = onlyNumbersEN.replace(/\D/g, "");
+
+        const strToNum = Number(regReplaceEN);
+
+        const installmentRule = (strToNum / 10).toFixed(2);
+
+        const numToStr = String(installmentRule);
+        console.log(numToStr, "NumToStr");
+
+        if (numToStr.length >= 10) {
+            const points = numToStr.substring(0, 2);
+            const rest = numToStr.substring(1, 4);
+            const cents = numToStr.substring(4, 6);
+            const formatedPrice = `${points}.${rest}.${cents}`;
+
+            const installmentRender = `Installment up to 10x with a minimum installment of $${formatedPrice}`;
+
+            installment.textContent = installmentRender;
+        } else if (numToStr.length === 6) {
+            const points = numToStr.substring(0, 1);
+            const rest = numToStr.substring(1, 3);
+            // const cents = numToStr.substring(4, 6);
+            const formatedPrice = `${points}.${rest}`;
+            console.log(formatedPrice);
+            const installmentRender = `Installment up to 10x with a minimum installment of $${formatedPrice}`;
+
+            installment.textContent = installmentRender;
+        } else if (numToStr.length === 7) {
+            const points = numToStr.substring(0, 2);
+            const rest = numToStr.substring(2, 4);
+            const formatedPrice = `${points}.${rest}`;
+            const installmentRender = `Installment up to 10x with a minimum installment of $${formatedPrice}`;
+
+            installment.textContent = installmentRender;
+        } else {
+            const points = numToStr.substring(0, 2);
+            const installmentRender = `Parcele em até 10x com parcela mínima de $0.${points}`;
+
+            installment.textContent = installmentRender;
+        }
     }
 });
